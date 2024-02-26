1390fe61b325e71bd2ff7aba7e769885dc87c49f
Ketpul <emiliqnnik07@abv.bg>
2024-02-26 19:06:58 +0200

    ServiceCollectionExtensions added to simplify Program.cs

---
+++ Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection sevices)
+        {
+            return sevices;
+        }
+        
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection sevices, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            sevices.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            sevices.AddDatabaseDeveloperPageExceptionFilter();
+
+            return sevices;
+        }
+        
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection sevices, IConfiguration config)
+        {
+            sevices
+                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return sevices;
+        }
+
+
+    }
+}
